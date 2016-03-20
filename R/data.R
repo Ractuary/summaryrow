a51578f9dbd43fa1390ce5258d11acf7c78f8851
merlinoa <merlinoa88@gmail.com>
2016-03-20 17:30:35 -0400

    created sample dataset

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Simple insurance sample data
+#' 
+#' @format A data frame with 3 ros and 4 variables
+#' \describe{
+#'   \item{year}{calendar year}
+#'   \item{exposure}{exposure}
+#'   \item{reported}{reported loss}
+#'   \item{ultimate}{ultimate loss}
+#' }
+"insurance_data"
